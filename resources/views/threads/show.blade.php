e1aa8fcb9aee070c0e22d6380502075ebd9f0b0b
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-05 20:39:21 +0700

    Only logged-in user can see reply form in thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,11 +5,12 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-headding">
+                    <div class="panel-heading">
                         <div>
                             <a href="#">{{ $thread->owner->name }}</a> posted: 
-                            {{ $thread->title }}</div>                    
-                    </div>
+                            {{ $thread->title }}
+                        </div>   
+                    </div>                 
                     <div class="panel-body">
                         <p>{{ $thread->body }}</p>
                     </div>
@@ -22,7 +23,8 @@
                 @foreach ($thread->replies as $reply)
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
+                            <a href="#">{{ $reply->owner->name }} </a>
+                            said {{ $reply->created_at->diffForHumans() }}
                         </div>
                         <div class="panel-body">
                             <p>{{ $reply->body }}</p>
@@ -31,6 +33,24 @@
                 @endforeach
             </div>
         </div>
+
+        @auth
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form action="{{ $thread->link() . '/replies' }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <textarea class="form-control" name="body" rows="5" placeholder="Have something to say?"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+            </div>
+        @endauth
+
+        @guest
+            <p class="text-center">Please <a href="/login">Sign in</a> to participate in the discussion!</p>
+        @endguest
     </div>    
 @endsection
 
