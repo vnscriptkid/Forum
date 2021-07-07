595df67c703e76ed2be3e70869bfbae77f569636
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-07 18:08:24 +0700

    Add replies_count to thread index page

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,11 +5,14 @@
         <div class="row">
             <div class="col-md-8">
                 @foreach ($threads as $thread)
-                    <h2>
-                        <a href="{{ $thread->link() }}">
-                            {{ $thread->title }}
-                        </a>
-                    </h2>
+                    <div class="row align-items-center">
+                        <h2 class="flex-fill">
+                            <a href="{{ $thread->link() }}">
+                                {{ $thread->title }}
+                            </a>
+                        </h2>
+                        <a href="{{ $thread->link() }}">{{ $thread->replies_count }} {{ Str::plural(' comment', $thread->replies_count ) }}</a>
+                    </div>
                     <p>{{ $thread->body }}</p>
                     <br>
                 @endforeach
