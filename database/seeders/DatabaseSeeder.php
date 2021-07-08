60233a8d101159f80673f17eabfadf9f0fe00862
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-08 14:17:59 +0700

    Update DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Favorite;
 use App\Models\Reply;
 use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -17,15 +19,46 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::transaction(function () {
+            create(User::class, 1, [
+                'email' => 'thanh@gmail.com',
+                'password' => bcrypt('12345678')
+            ]);
+
             create(Thread::class, 20);
 
             $threadIds = Thread::all()->pluck('id');
+            $userIds = User::all()->pluck('id');
 
             Reply::factory(200)->create([
                 'thread_id' => function () use ($threadIds) {
                     return $threadIds->random();
                 }
             ]);
+
+            $replyIds = Reply::all()->pluck('id');
+
+            collect(range(1, 500))->each(function () use ($userIds, $replyIds) {
+                $added = false;
+                while (!$added) {
+                    $userId = $userIds->random();
+                    $replyId = $replyIds->random();
+
+                    $favorite = Favorite::where([
+                        'user_id' => $userId,
+                        'favorited_id' => $replyId,
+                        'favorited_type' => 'App\Models\Reply'
+                    ]);
+
+                    if (!$favorite->exists()) {
+                        DB::table('favorites')->insert([
+                            'user_id' => $userId,
+                            'favorited_id' => $replyId,
+                            'favorited_type' => 'App\Models\Reply'
+                        ]);
+                        $added = true;
+                    }
+                }
+            });
         });
     }
 }
