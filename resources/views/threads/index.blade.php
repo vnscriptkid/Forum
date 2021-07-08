92bcacdf317a63757002d5633b3292492b8c2868
Thanh Nguyen <vnscriptkid@gmail.com>
2021-07-08 14:17:45 +0700

    Update UI for threads index page

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,16 +5,20 @@
         <div class="row">
             <div class="col-md-8">
                 @foreach ($threads as $thread)
-                    <div class="row align-items-center">
-                        <h2 class="flex-fill">
-                            <a href="{{ $thread->link() }}">
-                                {{ $thread->title }}
-                            </a>
-                        </h2>
-                        <a href="{{ $thread->link() }}">{{ $thread->replies_count }} {{ Str::plural(' comment', $thread->replies_count ) }}</a>
+                    <div class="card mb-3">
+                        <div class="card-header d-flex align-items-center">
+                            <div class="flex-fill">
+                                <h5 class="card-title">
+                                    <a href="{{ $thread->link() }}">
+                                        {{ $thread->title }}
+                                    </a>
+                                </h5>
+                                By <a href="#">{{ $thread->owner->name }}</a>
+                            </div>
+                            <a href="{{ $thread->link() }}">{{ $thread->replies_count }} {{ Str::plural(' comment', $thread->replies_count ) }}</a>
+                        </div>
+                        <p class="card-body">{{ $thread->body }}</p>
                     </div>
-                    <p>{{ $thread->body }}</p>
-                    <br>
                 @endforeach
             </div>
         </div>
